d529db54138404da0833a2d822f31b6efce7c3fc
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2026-02-27 07:00:50 -0500

    Update rss.js

--- api/rss.js
+++ api/rss.js
@@ -1,84 +1,9 @@
+import { buildRssXml } from "../buildRssXml.js";
+
 export default function handler(req, res) {
   const data = require("../data/podcast.json");
 
-  const channel = data.channel;
-  const episodes = data.episodes;
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0"
-     xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"
-     xmlns:atom="http://www.w3.org/2005/Atom"
-     xmlns:podcast="https://podcastindex.org/namespace/1.0">
-
-  <channel>
-
-    <title>${channel.title}</title>
-    <link>${channel.link}</link>
-    <language>${channel.language}</language>
-    <description><![CDATA[${channel.description}]]></description>
-
-    <atom:link href="${channel.feedUrl}" rel="self" type="application/rss+xml" />
-
-    <itunes:author>${channel.author}</itunes:author>
-    <itunes:summary>${channel.summary}</itunes:summary>
-
-    <itunes:owner>
-      <itunes:name>${channel.owner.name}</itunes:name>
-      <itunes:email>${channel.owner.email}</itunes:email>
-    </itunes:owner>
-
-    <itunes:image href="${channel.image}" />
-
-    <itunes:category text="${channel.category.main}">
-      <itunes:category text="${channel.category.sub}" />
-    </itunes:category>
-
-    <itunes:explicit>${channel.explicit}</itunes:explicit>
-    <itunes:type>${channel.type}</itunes:type>
-
-    <podcast:locked>${channel.locked}</podcast:locked>
-    <podcast:guid>${channel.podcastGuid}</podcast:guid>
-
-    <copyright>${channel.copyright}</copyright>
-    <lastBuildDate>${channel.lastBuildDate}</lastBuildDate>
-
-    ${episodes
-      .map(
-        (ep) => `
-    <item>
-      <title>${ep.title}</title>
-      <link>${ep.link}</link>
-      <description><![CDATA[${ep.description}]]></description>
-
-      <enclosure 
-        url="${ep.audioUrl}"
-        length="${ep.audioLength}"
-        type="${ep.audioType}"
-      />
-
-      <guid isPermaLink="false">${ep.guid}</guid>
-      <pubDate>${ep.pubDate}</pubDate>
-
-      <itunes:author>${ep.author}</itunes:author>
-      <itunes:subtitle>${ep.subtitle}</itunes:subtitle>
-      <itunes:summary>${ep.summary}</itunes:summary>
-      <itunes:duration>${ep.duration}</itunes:duration>
-      <itunes:explicit>${ep.explicit}</itunes:explicit>
-
-      <itunes:episode>${ep.episode}</itunes:episode>
-      <itunes:season>${ep.season}</itunes:season>
-      <itunes:episodeType>${ep.episodeType}</itunes:episodeType>
-
-      <itunes:image href="${ep.image}" />
-
-      <podcast:transcript url="${ep.transcript.url}" type="${ep.transcript.type}" />
-    </item>
-    `
-      )
-      .join("")}
-
-  </channel>
-</rss>`;
+  const xml = buildRssXml(data.channel, data.episodes);
 
   res.setHeader("Content-Type", "application/xml");
   res.status(200).send(xml);
